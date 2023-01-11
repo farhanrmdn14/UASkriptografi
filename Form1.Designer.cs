2f552ab28b76b3eae413ccd044e2908ade72d13b
Farhan Ramadhan <116280748+farhanrmdn14@users.noreply.github.com>
2023-01-12 00:23:29 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TBBL_AES_Encriptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtSwitch = new System.Windows.Forms.Button();
+            this.BtExecute = new System.Windows.Forms.Button();
+            this.Text2 = new System.Windows.Forms.RichTextBox();
+            this.Text1 = new System.Windows.Forms.RichTextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(245, 89);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(318, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Advanced Encryption Standard (AES)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("OCR A Extended", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(474, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ENCRYPTOR";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.BtSwitch);
+            this.groupBox1.Controls.Add(this.BtExecute);
+            this.groupBox1.Controls.Add(this.Text2);
+            this.groupBox1.Controls.Add(this.Text1);
+            this.groupBox1.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(35, 143);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(700, 292);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Main Program";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtSwitch
+            // 
+            this.BtSwitch.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtSwitch.Location = new System.Drawing.Point(312, 213);
+            this.BtSwitch.Name = "BtSwitch";
+            this.BtSwitch.Size = new System.Drawing.Size(75, 23);
+            this.BtSwitch.TabIndex = 3;
+            this.BtSwitch.Text = "Decript";
+            this.BtSwitch.UseVisualStyleBackColor = true;
+            this.BtSwitch.Click += new System.EventHandler(this.BtSwitch_Click);
+            // 
+            // BtExecute
+            // 
+            this.BtExecute.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtExecute.Location = new System.Drawing.Point(312, 113);
+            this.BtExecute.Name = "BtExecute";
+            this.BtExecute.Size = new System.Drawing.Size(75, 75);
+            this.BtExecute.TabIndex = 2;
+            this.BtExecute.Text = "Encrypt";
+            this.BtExecute.UseVisualStyleBackColor = true;
+            this.BtExecute.Click += new System.EventHandler(this.BtExecute_Click);
+            // 
+            // Text2
+            // 
+            this.Text2.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Text2.Location = new System.Drawing.Point(393, 46);
+            this.Text2.Name = "Text2";
+            this.Text2.ReadOnly = true;
+            this.Text2.Size = new System.Drawing.Size(299, 219);
+            this.Text2.TabIndex = 1;
+            this.Text2.Text = "";
+            // 
+            // Text1
+            // 
+            this.Text1.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Text1.Location = new System.Drawing.Point(7, 46);
+            this.Text1.Name = "Text1";
+            this.Text1.Size = new System.Drawing.Size(299, 219);
+            this.Text1.TabIndex = 0;
+            this.Text1.Text = "";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(595, 445);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(140, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "pandascode.wordpress.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(364, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Key";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(428, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 467);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "TBBL - AES Encryptor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtSwitch;
+        private System.Windows.Forms.Button BtExecute;
+        private System.Windows.Forms.RichTextBox Text2;
+        private System.Windows.Forms.RichTextBox Text1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace TBBL_AES_Encriptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            RijndaelManaged algo = new RijndaelManaged();
+            algo.KeySize = 256;
+                //algo.GenerateKey();
+                algo.GenerateIV();
+                kunci = algo.Key;
+                iv = algo.IV;
+                
+            
+        }
+
+        private void BtExecute_Click(object sender, EventArgs e)
+        {
+            if (BtExecute.Text == "Encrypt")
+            {
+                
+                kunci= System.Text.Encoding.UTF8.GetBytes(buatKunci(textBox1.Text));
+
+                byte[] hasil_byte=EnkripsiStringKeByte(Text1.Text,kunci,iv);
+
+                StringBuilder hasil_akhir = new StringBuilder();
+
+                foreach (byte data in hasil_byte)
+                {
+                    hasil_akhir.Append(data.ToString("x2"));
+                }
+                Text2.Text = hasil_akhir.ToString();
+                //textBox1.Text = hasil_akhir.ToString();
+
+            }
+            else if (BtExecute.Text == "Decrypt")
+            {
+                kunci = System.Text.Encoding.UTF8.GetBytes(buatKunci(textBox1.Text));
+
+                string cipher = Text1.Text;
+                byte[] chiperb = new byte[cipher.Length / 2];
+                for (int a = 0; a < cipher.Length / 2; a++)
+                {
+                    string byteValue = cipher.Substring(a * 2, 2);
+                    chiperb[a] = byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+
+                }
+                Text2.Text = DekripsiByteKeString(chiperb, kunci, iv);
+            }
+        }
+
+        static byte[] EnkripsiStringKeByte(string PlainText, byte[] Kunci, byte[] IV)
+        {
+            byte[] hasil_enkripsi;
+            if (PlainText == null || PlainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Kunci == null || Kunci.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            using (RijndaelManaged Algo = new RijndaelManaged())
+            {
+                Algo.Key = Kunci;
+                Algo.IV = IV;
+                Algo.Mode = CipherMode.CBC;
+                Algo.Padding = PaddingMode.Zeros;
+
+                ICryptoTransform enkrip = Algo.CreateEncryptor(Algo.Key, Algo.IV);
+
+                using (MemoryStream MS = new MemoryStream())
+                {
+                    using (CryptoStream CS = new CryptoStream(MS,enkrip,CryptoStreamMode.Write))
+                    {
+                        using(StreamWriter SW = new StreamWriter(CS))
+                        {
+                            SW.Write(PlainText);
+                        }
+                        hasil_enkripsi = MS.ToArray();
+                    }
+                }
+            }
+
+                return hasil_enkripsi;
+        }
+
+        static string DekripsiByteKeString(byte[] CipherText, byte[] Kunci, byte[] IV)
+        {
+            string hasilDekripsi=null;
+            if (CipherText == null || CipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Kunci == null || Kunci.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            using (RijndaelManaged Algo = new RijndaelManaged())
+            {
+                Algo.Key = Kunci;
+                Algo.IV = IV;
+                Algo.Mode = CipherMode.CBC;
+                Algo.Padding = PaddingMode.Zeros;
+
+                ICryptoTransform dekripsi = Algo.CreateDecryptor(Algo.Key, Algo.IV);
+
+                using (MemoryStream MS = new MemoryStream(CipherText))
+                {
+                    using (CryptoStream CS = new CryptoStream(MS, dekripsi, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader SR = new StreamReader(CS))
+                        {
+                            hasilDekripsi = SR.ReadToEnd();   
+                        }
+                    }
+                }
+            }
+            return hasilDekripsi;
+        }
+
+        private void BtSwitch_Click(object sender, EventArgs e)
+        {
+            if (BtExecute.Text == "Encrypt")
+            {
+                BtExecute.Text = "Decrypt";
+                BtSwitch.Text = "Encrypt";
+            }
+            else if (BtExecute.Text == "Decrypt")
+            {
+                BtSwitch.Text = "Decrypt";
+                BtExecute.Text = "Encrypt";
+            }
+            button1_Click(null, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Text1.Clear();Text2.Clear();textBox1.Clear(); 
+        }
+
+
+        private static byte[] kunci = new byte[32],iv = new byte[32];
+
+        public static string buatKunci(string s)
+        {
+            bool a = true; string d=null;
+            while (a)
+            {
+                if (s.Length <= 32)
+                { s = s + s; }
+                else { d=s.Remove(32);a = false; }
+            }
+            return d;
+        }
+    }
+}
